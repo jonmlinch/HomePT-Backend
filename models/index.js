73936e8edcc1003fe25ad38f6674f75f3ae924c5
AFresnedo <AFresnedo@users.noreply.github.com>
2018-09-19 15:33:52 -0700

    add exercises seeder

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 // create and/or connect to database
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/backend-pt',
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/homePT',
   { useNewUrlParser: true });
 
 // Deprecation warning: https://github.com/Automattic/mongoose/issues/6922
