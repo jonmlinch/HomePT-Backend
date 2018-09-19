1d00ba04593d5119c5384c1c3d14d9b1508c74c3
AFresnedo <AFresnedo@users.noreply.github.com>
2018-09-19 07:06:13 -0700

    psuedocode patch for assignments

--- controllers/AssignedExcercises.js
+++ controllers/AssignedExcercises.js
@@ -5,37 +5,17 @@ const db = require('../models');
 // load router to export routes to /index.js
 const router = express.Router();
 
-// get all excercises
-router.get('/', (req, res) => {
-  db.Exercise.find({})
-    .then(results => {
-      res.status(200).send({ exercises: results });
+router.patch('/', (req, res) => {
+  db.AssignedExcercise.findById(req.body.id)
+    .then(target => {
+      // TODO syntax
+      target = new db.AssignedExcercise(req.body);
+      // TODO handle error?
+      target.save();
+      res.status(200).send({ 'Successfully updated assignment });
     })
     .catch(err => {
-      res.status(400).send({ err: 'Undocumented err' });
-    });
-});
-
-router.get('/by/name', (req, res) => {
-  db.Exercise.findOne({
-    where: { name: req.body.name }
-  })
-    .then(result => {
-      res.status(200).send({ exercise: result });
-    })
-    .catch(err => {
-      res.status(400).send({ err: 'Undocumented err' });
-    });
-});
-
-router.post('/', (req, res) => {
-  db.Exercise.create(req.body)
-    .then(newEx => {
-      res.status(201).send({ success: 'Exercise created' });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(400).send({ err: 'Undocumented error' });
+      res.status(503).send({ err: 'database err' });
     });
 });
 
