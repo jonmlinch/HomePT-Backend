95cbacb86cd95a0b9272bd6405f08dd54354c3dc
AFresnedo <AFresnedo@users.noreply.github.com>
2018-09-19 08:19:14 -0700

    remove prescript id from assignex

--- models/AssignedExcercise.js
+++ models/AssignedExcercise.js
@@ -5,7 +5,6 @@ const mongoose = require('mongoose');
 
 const assignedExcerciseSchema = new mongoose.Schema({
   client: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  prescription: { type: mongoose.Schema.Types.ObjectId, ref: 'Prescription' },
   exercise: { type: mongoose.Schema.Types.ObjectId, ref: 'Exercise' },
   date: { type: Date, default: Date.now }
 });
